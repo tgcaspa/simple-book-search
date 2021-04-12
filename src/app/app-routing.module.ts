61848d238b520a1fd50a586a69a33363725c04a5
tgcaspa <cmplerec@gmail.com>
2021-04-12 23:49:57 +0300

    Add app route for welcome module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: () => import('./welcome/welcome.module').then(m => m.WelcomeModule),
+  },
+  {
     path: '**',
     redirectTo: '/',
     pathMatch: 'full'
