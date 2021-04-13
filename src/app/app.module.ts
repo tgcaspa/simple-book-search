a28f24f4b0e6b494a764d7f03e4ca5b67afbd2da
tgcaspa <cmplerec@gmail.com>
2021-04-13 18:34:29 +0300

    Import auth module in app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { NG_ENTITY_SERVICE_CONFIG } from '@datorama/akita-ng-entity-service';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './common/auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AuthModule,
     AppRoutingModule,
     environment.production ? [] : AkitaNgDevtools.forRoot()
   ],
