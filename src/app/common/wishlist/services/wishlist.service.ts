332e7e721ecdf3c22574fd9f878a71f7bdb1368e
tgcaspa <cmplerec@gmail.com>
2021-04-18 00:30:56 +0300

    Create common wishlist module with service.

---
+++ src/app/common/wishlist/services/wishlist.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+import { BookItem } from '../../books/state/book.model';
+import { WishlistStore } from '../state/wishlist.store';
+
+
+@Injectable()
+export class WishlistService {
+
+  constructor(private store: WishlistStore) { }
+
+  add(bookItem: BookItem): void {
+    return this.store.add({ ...bookItem, isWishlish: true });
+  }
+
+  remove(bookItem: BookItem): any {
+    return this.store.remove(bookItem.id);
+  }
+}
---
+++ src/app/common/wishlist/wishlist/wishlist.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+import { WishlistService } from './../services/wishlist.service';
+
+@NgModule({
+  providers: [
+    WishlistService
+  ]
+})
+export class WishlistCommonModule { }
