ec4418aae70f6fbef3e9cd13751d942a7c27077f
tgcaspa <cmplerec@gmail.com>
2021-04-17 15:44:31 +0300

    Add more interface VolumeInfo fields.

--- src/app/common/books/state/book.model.ts
+++ src/app/common/books/state/book.model.ts
@@ -10,16 +10,20 @@ export interface VolumeInfo {
   title: string;
   subtitle: string;
   authors: string[];
+  categories: string[];
   description: string;
-  averageRating: number;
+  language: number;
   imageLinks: {
     thumbnail: string
   };
+  averageRating?: number;
+  pageCount?: number;
 }
 
 export interface BookItem {
   id: string;
   volumeInfo: VolumeInfo;
+  isWishlish?: boolean;
 }
 
 export interface BooksState extends EntityState<BookItem, BookItem['id']> { }
@@ -37,12 +41,16 @@ export const extractBookData = (data: BookItem): BookItem => ({
     title: data?.volumeInfo?.title,
     subtitle: data?.volumeInfo?.subtitle,
     authors: data?.volumeInfo?.authors,
+    categories: data?.volumeInfo?.categories,
     description: data?.volumeInfo?.description,
     averageRating: data?.volumeInfo?.averageRating,
+    language: data?.volumeInfo?.language,
     imageLinks: {
       thumbnail: data?.volumeInfo?.imageLinks?.thumbnail
-    }
-  }
+    },
+    pageCount: data?.volumeInfo?.pageCount
+  },
+  isWishlish: data?.isWishlish === true
 });
 
 export const extractBookEntities = (response: BooksVolumesResponse): BookItem[] => (response?.items || []).map(extractBookData);
