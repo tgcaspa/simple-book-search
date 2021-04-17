1e23a6d33e5c675b5a31b78ab22d916d31fcdde3
tgcaspa <cmplerec@gmail.com>
2021-04-17 16:04:25 +0300

    Clean unused import in wishlist query.

--- src/app/wishlist/state/wishlist.query.ts
+++ src/app/wishlist/state/wishlist.query.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { QueryEntity } from '@datorama/akita';
-import { Observable } from 'rxjs';
 
 import { BooksState } from './../../common/books/state/book.model';
 import { WishlistStore } from './wishlist.store';
