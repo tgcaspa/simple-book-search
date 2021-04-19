cc0cf32a709f93269204e101e19b06ca4decf5bd
tgcaspa <cmplerec@gmail.com>
2021-04-19 11:05:27 +0300

    Remove search store to be persist.

--- src/app/app.store.ts
+++ src/app/app.store.ts
@@ -2,7 +2,6 @@ import { persistState } from '@datorama/akita';
 
 import { environment } from 'src/environments/environment';
 import { STORE_NAME_USER } from './common/user/state/user.store';
-import { STORE_NAME_SEARCH } from './search/state/search/search.store';
 import { STORE_NAME_WISHLIST } from './common/wishlist/state/wishlist.store';
 
 export const APP_STORE_NAME = `${environment.appName}Stores`;
@@ -12,7 +11,6 @@ export const registerAppStores = () => persistState({
   storage: sessionStorage,
   include: [
     STORE_NAME_USER,
-    STORE_NAME_SEARCH,
     STORE_NAME_WISHLIST
   ]
 });
