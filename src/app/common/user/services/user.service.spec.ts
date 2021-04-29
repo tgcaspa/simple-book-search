4e5b5f72358e0f171f2162b3b5d7638d5bcad328
tgcaspa <cmplerec@gmail.com>
2021-04-30 00:10:13 +0300

    Fix user.service.spec.ts.

--- src/app/common/user/services/user.service.spec.ts
+++ src/app/common/user/services/user.service.spec.ts
@@ -1,16 +1,41 @@
 import { TestBed } from '@angular/core/testing';
 
+import { UserStore } from '../state/user.store';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
-  let service: UserService;
+  let userStoreSpy: jasmine.SpyObj<UserStore>;
+  let userService: UserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: UserStore, useValue: jasmine.createSpyObj('UserStore', ['update']) },
+        UserService
+      ]
+    });
+    userStoreSpy = TestBed.inject(UserStore) as jasmine.SpyObj<UserStore>;
+    userService = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(userService).toBeTruthy();
+  });
+
+  describe('#loadUser', () => {
+
+    it('should update store with given user\'s data', () => {
+      const user = {
+        username: 'Dmitry Hershkovich'
+      };
+      userService.loadUser(user).subscribe(
+        result => {
+          expect(userStoreSpy.update).toHaveBeenCalledTimes(1);
+
+          expect(result).toEqual(user);
+        },
+        fail
+      );
+    });
   });
 });
