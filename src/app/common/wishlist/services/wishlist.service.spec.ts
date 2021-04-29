e5bb6757a00cea7b08075f702212f5dcf404dc51
tgcaspa <cmplerec@gmail.com>
2021-04-30 00:25:21 +0300

    Fix wishlist.service.spec.ts.

--- src/app/common/wishlist/services/wishlist.service.spec.ts
+++ src/app/common/wishlist/services/wishlist.service.spec.ts
@@ -3,14 +3,16 @@ import { TestBed } from '@angular/core/testing';
 import { WishlistService } from './wishlist.service';
 
 describe('WishlistService', () => {
-  let service: WishlistService;
+  let wishlistService: WishlistService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(WishlistService);
+    TestBed.configureTestingModule({
+      providers: [WishlistService]
+    });
+    wishlistService = TestBed.inject(WishlistService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(wishlistService).toBeTruthy();
   });
 });
