2d790f46b6088a96e0b67585abd2f1defeac1b1d
tgcaspa <cmplerec@gmail.com>
2021-05-01 21:56:37 +0300

    Add unit test for #getMenuItems in layout-header.component.spec.ts

--- src/app/layout/layout-header/layout-header.component.spec.ts
+++ src/app/layout/layout-header/layout-header.component.spec.ts
@@ -1,18 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Route, Router } from '@angular/router';
 import { of } from 'rxjs';
 
 import { AuthService } from './../../common/auth/services/auth.service';
 import { LayoutHeaderComponent } from './layout-header.component';
 
+const routerConfig = [
+  {
+    path: 'welcome',
+    data: {
+      title: 'Welcome',
+      displayInMenu: false
+    }
+  },
+  {
+    path: 'search',
+    data: {
+      title: 'Search',
+      displayInMenu: true
+    }
+  },
+  {
+    path: 'wishlist',
+    data: {
+      title: 'Wishlist',
+      displayInMenu: true
+    }
+  },
+];
+
 describe('LayoutHeaderComponent', () => {
   let authServiceSpy: jasmine.SpyObj<AuthService>;
+  let routerServiceSpy: Router;
   let component: LayoutHeaderComponent;
   let fixture: ComponentFixture<LayoutHeaderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [LayoutHeaderComponent],
       providers: [
+        { provide: Router, useValue: jasmine.createSpyObj('Router', [], { config: routerConfig }) },
         { provide: AuthService, useValue: jasmine.createSpyObj('AuthService', ['logout']) },
       ]
     })
@@ -28,6 +57,27 @@ describe('LayoutHeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#getMenuItems', () => {
+    let menuItems: Route[];
+
+    beforeEach(() => {
+      menuItems = component.getMenuItems();
+    });
+
+    it('should get menu items that defined as displayInMenu in route config', () => {
+      expect(menuItems.length).toBe(2);
+
+      const hasWelcomeItem = menuItems.find(item => item.path === 'welcome');
+      expect(hasWelcomeItem).toBeUndefined();
+
+      const hasSearchItem = menuItems.find(item => item.path === 'search');
+      expect(hasSearchItem).toBeDefined();
+
+      const hasWishlistItem = menuItems.find(item => item.path === 'wishlist');
+      expect(hasWishlistItem).toBeDefined();
+    });
   });
 
   describe('#onLogout', () => {
