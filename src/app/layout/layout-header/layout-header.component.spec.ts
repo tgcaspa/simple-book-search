fef39877c7fabf58a96ecd13d4af9d271d8759a9
tgcaspa <cmplerec@gmail.com>
2021-05-01 21:19:44 +0300

    Fix layout-header.component.spec.ts.

--- src/app/layout/layout-header/layout-header.component.spec.ts
+++ src/app/layout/layout-header/layout-header.component.spec.ts
@@ -1,19 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
+import { AuthService } from './../../common/auth/services/auth.service';
 import { LayoutHeaderComponent } from './layout-header.component';
 
 describe('LayoutHeaderComponent', () => {
+  let authServiceSpy: jasmine.SpyObj<AuthService>;
   let component: LayoutHeaderComponent;
   let fixture: ComponentFixture<LayoutHeaderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LayoutHeaderComponent ]
+      declarations: [LayoutHeaderComponent],
+      providers: [
+        { provide: AuthService, useValue: jasmine.createSpyObj('AuthService', ['logout']) },
+      ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    authServiceSpy = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
     fixture = TestBed.createComponent(LayoutHeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,5 +28,16 @@ describe('LayoutHeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#onLogout', () => {
+
+    it('should logout', () => {
+      authServiceSpy.logout.and.returnValue(of(true));
+
+      component.onLogout();
+
+      expect(authServiceSpy.logout).toHaveBeenCalledTimes(1);
+    });
   });
 });
