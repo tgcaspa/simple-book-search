1002ef500bf18c70d08e007002826e60c4054352
דמיטרי הרשקוביץ <dmitryh@migdal-group.co.il>
2021-05-03 09:55:25 +0300

    Add unit test for method #setActive in books.service.spec.ts

--- src/app/common/books/services/books.service.spec.ts
+++ src/app/common/books/services/books.service.spec.ts
@@ -1,16 +1,43 @@
 import { TestBed } from '@angular/core/testing';
 
+import { BookItem } from '../state/book.model';
 import { BooksService } from './books.service';
+import { BooksStore } from "src/app/common/books/state/books.store";
+
+const bookItem: BookItem = {
+  id: '--0rAAAAIAAJ',
+  volumeInfo: {
+    title: 'Proceedings of the Senate Sitting for the Trial of William W. Belknap, Late Secretary of War, on the Articles of Impeachment Exhibited by the House of Representatives',
+    subtitle: 'subtitle',
+    authors: [
+      'William Worth Belknap'
+    ],
+    categories: [
+      'Trials (Impeachement)'
+    ],
+    description: 'description',
+    averageRating: 3,
+    language: 'en',
+    imageLinks: {
+      thumbnail: 'http://books.google.com/books/content?id=--0rAAAAIAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+    },
+    pageCount: 1166
+  },
+  isWishlish: false
+};
 
 describe('BooksService', () => {
+  let booksStoreSpy: jasmine.SpyObj<BooksStore>;
   let booksService: BooksService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
+        { provide: BooksStore, useValue: jasmine.createSpyObj('BooksStore', ['setActive']) },
         BooksService
       ]
     });
+    booksStoreSpy = TestBed.inject(BooksStore) as jasmine.SpyObj<BooksStore>;
     booksService = TestBed.inject(BooksService);
   });
 
@@ -20,6 +47,11 @@ describe('BooksService', () => {
 
   describe('#setActive', () => {
 
+    it('should set active book item by id', () => {
+      booksService.setActive(bookItem);
+
+      expect(booksStoreSpy.setActive).toHaveBeenCalledOnceWith(bookItem.id);
+    });
   });
 
   describe('#removeActive', () => {
