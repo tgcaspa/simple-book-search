08dbced078a2d0ee62a3e70d45fb0aa4759ab130
דמיטרי הרשקוביץ <dmitryh@migdal-group.co.il>
2021-05-03 10:26:37 +0300

    Fix wishlist.component.spec.ts.

--- src/app/wishlist/wishlist.component.spec.ts
+++ src/app/wishlist/wishlist.component.spec.ts
@@ -1,6 +1,48 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { WishlistComponent } from './wishlist.component';
+import { WishlistQuery } from '../common/wishlist/state/wishlist.query';
+import { BookItem } from '../common/books/state/book.model';
+import { ResultsContainerModule } from "src/app/common/results-container/results-container.module";
+import { BooksModule } from "src/app/common/books/books.module";
+
+const wishlistItems: BookItem[] = [
+  {
+    id: 'KODWBVNNPLOE',
+    volumeInfo: {
+      title: 'Book 1',
+      subtitle: 'subtitle 1',
+      authors: [],
+      categories: [],
+      description: 'description 1',
+      averageRating: 3,
+      language: 'en',
+      imageLinks: {
+        thumbnail: 'http://books.google.com/books/content?id=KODWBVNNPLOE&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+      },
+      pageCount: 1166
+    },
+    isWishlish: true
+  },
+  {
+    id: 'VHOEAAOOPLIJ',
+    volumeInfo: {
+      title: 'Book 2',
+      subtitle: 'subtitle 2',
+      authors: [],
+      categories: [],
+      description: 'description 2',
+      averageRating: 3,
+      language: 'en',
+      imageLinks: {
+        thumbnail: 'http://books.google.com/books/content?id=VHOEAAOOPLIJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+      },
+      pageCount: 1166
+    },
+    isWishlish: true
+  }
+];
 
 describe('WishlistComponent', () => {
   let component: WishlistComponent;
@@ -8,7 +50,14 @@ describe('WishlistComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ WishlistComponent ]
+      imports: [
+        BooksModule,
+        ResultsContainerModule
+      ],
+      declarations: [WishlistComponent],
+      providers: [
+        {provide: WishlistQuery, useValue: jasmine.createSpyObj('WishlistQuery', {selectAll: of(wishlistItems)})}
+      ]
     })
     .compileComponents();
   });
@@ -21,5 +70,12 @@ describe('WishlistComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should init books items on init', () => {
+    component.books$.subscribe(
+      (bookItems: BookItem[]) => expect(bookItems).toEqual(wishlistItems),
+      fail
+    );
   });
 });
