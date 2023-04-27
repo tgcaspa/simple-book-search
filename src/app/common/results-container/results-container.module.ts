0b53a061f57fa75336baa768413c12c1619f0a78
tgcaspa <cmplerec@gmail.com>
2023-04-27 09:52:35 +0300

    Fix rating styles

--- src/app/common/results-container/results-container.module.ts
+++ src/app/common/results-container/results-container.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { RatingModule } from 'ngx-bootstrap/rating';
 
 import { WishlistCommonModule } from '../wishlist/wishlist/wishlist.module';
 import { ResultsContainerComponent } from './results-container.component';
@@ -13,14 +14,15 @@ import { VolumeInfoModalComponent } from './volume-info-modal/volume-info-modal.
   declarations: [
     ResultsContainerComponent,
     VolumeInfoCardComponent,
-    VolumeInfoModalComponent
+    VolumeInfoModalComponent,
   ],
   imports: [
     CommonModule,
     FormsModule,
     ModalModule.forRoot(),
     PaginationModule.forRoot(),
-    WishlistCommonModule
+    WishlistCommonModule,
+    RatingModule,
   ],
   exports: [
     ResultsContainerComponent,
